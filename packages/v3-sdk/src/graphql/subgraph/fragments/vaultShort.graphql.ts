3204e5c0e793c2acf64b52efc1cdee9e73bff560
MikeDiam <info@mdiamond.ru>
2023-08-17 14:38:16 +0300

    [vault actions] Remove gql types

--- packages/v3-sdk/src/graphql/subgraph/fragments/vaultShort.graphql.ts
+++
@@ -1,3 +0,0 @@
-const vaultShortFragment = 'fragment VaultShort on Vault { address: id performance: score imageUrl mevEscrow isPrivate createdAt totalAssets displayName description whitelister avgRewardPerAsset}'
-
-export { vaultShortFragment }
