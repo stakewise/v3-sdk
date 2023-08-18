12a17ff59072b65b11c1515d5ed418240dfee1ef
MikeDiam <info@mdiamond.ru>
2023-08-18 11:54:41 +0300

    [vault actions] Update constants

--- packages/v3-sdk/src/constants.ts
+++ packages/v3-sdk/src/constants.ts
@@ -3,14 +3,10 @@ const url = {
   subgraph: 'https://graph-testnet.stakewise.io/subgraphs/name/stakewise/stakewise',
 }
 
-const etherscan = 'https://goerli.etherscan.io'
-const beaconchain = 'https://goerli.beaconcha.in'
 const sharedMevEscrow = '0xE33E8b756Cc5F4B622ce9A0d29c392F5999DB547'
 
 
 export default {
   url,
-  etherscan,
-  beaconchain,
   sharedMevEscrow,
 }
