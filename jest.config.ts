a606c156c225fda40d428d6cad2dd62de9ac7f19
Andrey Kopylov <programmer.andrey@gmail.com>
2023-09-13 19:25:01 +0500

    [config-sdk] git actions (test 1)

--- jest.config.ts
+++ jest.config.ts
@@ -5,19 +5,22 @@ const config: Config = {
   rootDir: '.',
   verbose: true,
   preset: 'ts-jest',
+  resetMocks: true,
+  testEnvironment: 'node',
+  testMatch: [ '**/*.spec.ts' ],
+  collectCoverageFrom: [ 'src/**/*.ts' ],
+  moduleFileExtensions: [ 'json', 'js', 'ts' ],
+  modulePathIgnorePatterns: [ '<rootDir>/src/helpers' ],
   transform: {
     '\\.[jt]sx?$': 'ts-jest',
+  },
+  globals: {
+    isolatedModules: true,
   },
   moduleNameMapper: {
     '^helpers$': require.resolve('./src/helpers/index.ts'),
     '^graphql$': require.resolve('./src/graphql/index.ts'),
   },
-  modulePathIgnorePatterns: [ '<rootDir>/src/helpers' ],
-  moduleFileExtensions: [ 'json', 'js', 'ts' ],
-  collectCoverageFrom: [ 'src/**/*.ts' ],
-  testMatch: [ '**/*.spec.ts' ],
-  testEnvironment: 'node',
-  resetMocks: true,
 }
 
 
