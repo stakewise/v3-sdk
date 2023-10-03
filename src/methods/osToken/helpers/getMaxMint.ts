f1feab7ca6f11c6f6772516df85dbfb5d60d2b77
Andrey Kopylov <programmer.andrey@gmail.com>
2023-10-03 18:15:09 +0500

    [validate-methods] create getMaxMinte

---
+++ src/methods/osToken/helpers/getMaxMint.ts
@@ -0,0 +1,40 @@
+import { BigDecimal } from 'helpers'
+
+
+type GetMaxMintInput = {
+  ltvPercent: bigint
+  mintedShares: bigint
+  stakedAssets: bigint
+  contracts: StakeWise.Contracts
+}
+
+const getMaxMint = async (values: GetMaxMintInput) => {
+  const { contracts, ltvPercent, mintedShares, stakedAssets } = values
+
+  if (ltvPercent <= 0 || stakedAssets <= 0) {
+    return 0n
+  }
+
+  const rewardPerSecond = await contracts.tokens.mintToken.avgRewardPerSecond()
+  const avgRewardPerHour = rewardPerSecond * 60n * 60n
+
+  const result = new BigDecimal(stakedAssets)
+    .multiply(ltvPercent)
+    .divide(10_000)
+    .minus(avgRewardPerHour)
+    .decimals(0)
+    .toString()
+
+  let maxMintShares = await contracts.tokens.mintToken.convertToShares(result)
+
+  if (maxMintShares > mintedShares) {
+    maxMintShares = maxMintShares - mintedShares
+
+    return maxMintShares
+  }
+
+  return 0n
+}
+
+
+export default getMaxMint
