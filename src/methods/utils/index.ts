0aa66f0903017bb4318655cc1c05d70f383eaf7c
CAst <programmer.andrey@gmail.com>
2024-01-04 13:09:21 +0500

    Small improves (#55)
    * [small-improves] remove getRewardsPerYear
    
    * [small-improves] add getGas to main class

--- src/methods/utils/index.ts
+++ src/methods/utils/index.ts
@@ -1,3 +1,2 @@
-export { default as getRewardsPerYear } from './getRewardsPerYear'
 export { default as getSwiseUsdPrice } from './getSwiseUsdPrice'
 export { default as getTransactions } from './getTransactions'
