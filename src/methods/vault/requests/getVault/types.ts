29a1e88744b9024707bf26e9d92ecd3f3098f18b
dfkadyr <dfkadyr@gmail.com>
2024-03-13 10:20:23 +0300

    [vault-version]: initial ui, add vault version to sdk

--- src/methods/vault/requests/getVault/types.ts
+++ src/methods/vault/requests/getVault/types.ts
@@ -3,9 +3,10 @@ import type { VaultQueryPayload } from '../../../../graphql/subgraph/vault'
 
 export type ModifiedVault = Omit<
   VaultQueryPayload['vault'],
-  'admin' | 'address' | 'mevEscrow' | 'keysManager' | 'apy' | 'performance' | 'createdAt' | 'blocklistCount' | 'whitelistCount'
+  'admin' | 'address' | 'mevEscrow' | 'keysManager' | 'apy' | 'performance' | 'createdAt' | 'blocklistCount' | 'whitelistCount' | 'version'
 > & {
   apy: number
+  version: number
   createdAt: number
   vaultAdmin: string
   performance: number
