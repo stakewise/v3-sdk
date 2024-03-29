6b1d1fc9f2eaa1de7b81caf2912df724ac8e3887
MikeDiam <info@mdiamond.ru>
2024-03-29 11:23:50 +0300

    [sdk vault methods] update validation

--- src/methods/vault/transactions/operate/util/params/getBlocklistParams.ts
+++ src/methods/vault/transactions/operate/util/params/getBlocklistParams.ts
@@ -1,5 +1,4 @@
 import validateList from './validateList'
-import { validateArgs } from '../../../../../../utils'
 import { vaultMulticall } from '../../../../../../contracts'
 
 
@@ -10,19 +9,10 @@ export type UpdateBlocklistParams = {
   }>
 }
 
-const validateBlocklist = (blocklist: UpdateBlocklistParams['blocklist']) => {
-  const isValid = validateList(blocklist)
-
-  if (!isValid) {
-    throw new Error('The "blocklist" argument must be an array of objects with "address" and "isNew" properties')
-  }
-}
-
 const getParams = (values: UpdateBlocklistParams) => {
   const { blocklist } = values
 
-  validateArgs.array({ blocklist })
-  validateBlocklist(blocklist)
+  validateList({ blocklist })
 
   const params: Parameters<typeof vaultMulticall>[0]['request']['params'] = blocklist.map(({ address, isNew }) => ({
     method: 'updateBlocklist',
--- src/methods/vault/transactions/operate/util/params/getWhitelistParams.ts
+++ src/methods/vault/transactions/operate/util/params/getWhitelistParams.ts
@@ -1,5 +1,4 @@
 import validateList from './validateList'
-import { validateArgs } from '../../../../../../utils'
 import { vaultMulticall } from '../../../../../../contracts'
 
 
@@ -10,19 +9,10 @@ export type UpdateWhitelistParams = {
   }>
 }
 
-const validateWhitelist = (whitelist: UpdateWhitelistParams['whitelist']) => {
-  const isValid = validateList(whitelist)
-
-  if (!isValid) {
-    throw new Error('The "whitelist" argument must be an array of objects with "address" and "isNew" properties')
-  }
-}
-
 const getParams = (values: UpdateWhitelistParams) => {
   const { whitelist } = values
 
-  validateArgs.array({ whitelist })
-  validateWhitelist(whitelist)
+  validateList({ whitelist })
 
   const params: Parameters<typeof vaultMulticall>[0]['request']['params'] = whitelist.map(({ address, isNew }) => ({
     method: 'updateWhitelist',
