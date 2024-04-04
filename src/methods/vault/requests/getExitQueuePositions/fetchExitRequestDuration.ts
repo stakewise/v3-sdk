cc1c36cd893d39bfcb135a8be2b877c1609f5536
dfkadyr <dfkadyr@gmail.com>
2024-04-04 13:59:49 +0300

    [duration]: PR improve

--- src/methods/vault/requests/getExitQueuePositions/fetchExitRequestDuration.ts
+++ src/methods/vault/requests/getExitQueuePositions/fetchExitRequestDuration.ts
@@ -22,7 +22,7 @@ const fetchExitRequestDuration = (values: FetchExitQueuePositionsInput) => {
         return 0
       }
 
-      const durations = data.exitRequests.map(request => request.duration)
+      const durations = data.exitRequests.map((request) => request.duration)
 
       return Math.max(...durations)
     },
