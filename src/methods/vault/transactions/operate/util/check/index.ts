53a102f79ab2b48d57578c304a62bef3728e5999
CAst <programmer.andrey@gmail.com>
2024-04-08 17:42:00 +0500

    Roles (#91)
    * [roles] add setDepositDataRoot to vault
    
    * [roles] renames & improves
    
    * [roles] setDepositDataManager logic
    
    * [roles] change readme
    
    * [roles] setValidatorsManager logic
    
    * [roles] improves
    
    * [roles] add JSdoc base for methods
    
    * [roles] fix

--- src/methods/vault/transactions/operate/util/check/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as checkAdminAccess } from './checkAdminAccess'
-export { default as checkKeysManagerAccess } from './checkKeysManagerAccess'
-export { default as checkWhitelisterAccess } from './checkWhitelisterAccess'
-export { default as checkBlocklistManagerAccess } from './checkBlocklistManagerAccess'
