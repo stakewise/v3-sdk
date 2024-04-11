8a6c174bbb373cd6e301d2d204e5df9683008e64
dfkadyr <dfkadyr@gmail.com>
2024-04-11 13:07:43 +0300

    [fallback]: add sessionStorage logic

--- src/modules/gql-module/graphqlFetch.ts
+++ src/modules/gql-module/graphqlFetch.ts
@@ -6,6 +6,17 @@ type ExtendedFetchInput<Data, Variables, ModifiedData> = FetchInput<Data, Variab
   retryCount?: number
 }
 
+const sessionErrorUrls = 'errorUrls'
+
+const saveErrorUrlToSessionStorage = (url: string) => {
+  const errorUrls = JSON.parse(sessionStorage.getItem(sessionErrorUrls) || '[]')
+
+  if (!errorUrls.includes(url)) {
+    errorUrls.push(url)
+    sessionStorage.setItem(sessionErrorUrls, JSON.stringify(errorUrls))
+  }
+}
+
 const graphqlFetch = <Data, Variables, ModifiedData>(
   options: ExtendedFetchInput<Data, Variables, ModifiedData>
 ): AbortRequest<Data, ModifiedData> => {
@@ -16,11 +27,14 @@ const graphqlFetch = <Data, Variables, ModifiedData>(
     .replace(/[({].*/, '')
     .trim()
 
-  const isUrlArray = Array.isArray(url)
-  const primaryUrl = isUrlArray ? url[0] : url
+  const isUrlArrayValid = Array.isArray(url) && url.length > 1
+  const primaryUrl = isUrlArrayValid ? url[0] : url
+  const backupUrl = isUrlArrayValid ? url[1] : null
 
+  const errorUrls = JSON.parse(sessionStorage.getItem(sessionErrorUrls) || '[]')
+  const validUrl = errorUrls.includes(primaryUrl) ? backupUrl : primaryUrl
   const opName = operationName ? `?opName=${operationName}` : ''
-  const requestUrl = `${primaryUrl}${opName}`
+  const requestUrl = `${validUrl}${opName}`
 
   return new AbortRequest<Data, ModifiedData>(requestUrl, {
     method: 'POST',
@@ -36,9 +50,9 @@ const graphqlFetch = <Data, Variables, ModifiedData>(
       ? modifyResult(data)
       : data as unknown as ModifiedData,
     onError: (error: Error | any) => {
-      const backupUrl = isUrlArray ? url[1] : null
-
       if (backupUrl && retryCount < 1) {
+        saveErrorUrlToSessionStorage(primaryUrl)
+
         return graphqlFetch({
           ...options,
           url: backupUrl,
