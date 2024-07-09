a226f26d5a9757aceff0f1a9843890e71ee8099b
Andrey Kopylov <programmer.andrey@gmail.com>
2024-07-09 17:37:10 +0500

    [gnosis-fixes] improve handleCallStatic

--- src/contracts/multicall/util/handleCallStatic.ts
+++ src/contracts/multicall/util/handleCallStatic.ts
@@ -8,24 +8,30 @@ type Input = {
   userAddress: string
 }
 
+// When running callStatic, we want to get result data, but not from these methods
+const deleteResultFromMethods = [
+  'updateState',
+  'swapXdaiToGno',
+]
+
 const handleCallStatic = async ({ contract, multicallParams, userAddress }: Input) => {
   const calls = getMulticallCalls({ multicallParams, contract })
   const result = await contract.multicall.staticCall(calls, { from: userAddress })
 
+  const idexesToDeleteResult: number[] = []
+
   const formattedResult = result.map((data: any, index: number) => {
     const { method } = multicallParams[index]
+
+    if (deleteResultFromMethods.includes(method)) {
+      idexesToDeleteResult.push(index)
+    }
 
     // @ts-ignore: TS has limitations when dealing with overloads
     return contract.interface.decodeFunctionResult(method, data)
   })
 
-  const [ _, ...rest ] = formattedResult
-
-  if (multicallParams[0]?.method === 'updateState') {
-    return rest
-  }
-
-  return formattedResult
+  return formattedResult.filter((_, index) => !idexesToDeleteResult.includes(index))
 }
 
 
