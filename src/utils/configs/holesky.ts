22b77192c5ce4ed3e42dcadb1154052b8a994e4c
CAst <programmer.andrey@gmail.com>
2024-07-18 12:07:33 +0500

    [new-holesky-addresses] set new (#140)

--- src/utils/configs/holesky.ts
+++ src/utils/configs/holesky.ts
@@ -17,8 +17,8 @@ export default {
       swise: '0x484871C6D54a3dAEBeBBDB0AB7a54c97D72986Bb',
       mintToken: '0xF603c5A3F774F05d4D848A9bB139809790890864',
       depositToken: ZeroAddress,
-      v2RewardToken: '0xd57c19f20168406d162852515030e00e49bB7781',
-      v2StakedToken: '0xFb534BB912Eb83b7b629329195b8DF798Ea325b2',
+      v2RewardToken: '0x2ee2E20702B5881a1171c5dbEd01C3d1e49Bf632',
+      v2StakedToken: '0x91167861c590Fd68bEbE662951fBE30C9B23D759',
     },
     base: {
       keeper: '0xB580799Bf7d62721D1a523f0FDF2f5Ed7BA4e259',
