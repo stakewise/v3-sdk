d5539619218be5256b19d32492cc3edecfb27b87
MikeDiam <info@mdiamond.ru>
2024-07-29 17:10:09 +0300

    [vault create sdk] update vault create

--- src/methods/vault/transactions/util/metadata/uploadMetadata/index.ts
+++ src/methods/vault/transactions/util/metadata/uploadMetadata/index.ts
@@ -15,21 +15,37 @@ type Input = UploadMetadataInput & {
 const uploadMetadata = async (input: Input) => {
   const { image = '', displayName = '', description = '', options } = input
 
+  if (image) {
+    validateArgs.image(image)
+  }
+
+  validateArgs.maxLength({
+    displayName: { value: displayName, length: 30 },
+    description: { value: description, length: 1000 },
+  })
+
   validateArgs.string({ image, displayName, description })
 
   if (image || displayName || description) {
-    const data = await graphql.backend.vault.submitUploadMetadataMutation({
-      url: apiUrls.getBackendUrl(options),
-      variables: {
-        payload: {
-          image,
-          displayName,
-          description,
+    try {
+      const data = await graphql.backend.vault.submitUploadMetadataMutation({
+        url: apiUrls.getBackendUrl(options),
+        variables: {
+          payload: {
+            image,
+            displayName,
+            description,
+          },
         },
-      },
-    })
+      })
 
-    return data?.uploadMetadata?.ipfsHash
+      return data?.uploadMetadata?.ipfsHash
+    }
+    catch (error) {
+      const errorCode = Array.isArray(error) ? error[0]?.extensions?.code : null
+
+      return Promise.reject(errorCode || error)
+    }
   }
 
   return null
