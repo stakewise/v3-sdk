c6f49d8b799c3fba2a4ed383f0c23ad5ead39147
MikeDiam <info@mdiamond.ru>
2024-07-30 13:15:14 +0300

    [vault create sdk] update blocklist

--- src/utils/getVaultFactory.ts
+++ src/utils/getVaultFactory.ts
@@ -11,11 +11,11 @@ const getVaultFactory = ({ vaultType, contracts, isErc20 }: Input) => {
   const vaultFactories = isErc20 ? {
     [VaultType.Default]: contracts.factories.erc20Vault,
     [VaultType.Private]: contracts.factories.erc20PrivateVault,
-    // [VaultType.Blocklist]: contracts.factories.erc20BlocklistVault,
+    [VaultType.Blocklist]: contracts.factories.erc20BlocklistVault,
   } : {
     [VaultType.Default]: contracts.factories.vault,
     [VaultType.Private]: contracts.factories.privateVault,
-    // [VaultType.Blocklist]: contracts.factories.blocklistVault,
+    [VaultType.Blocklist]: contracts.factories.blocklistVault,
   }
 
   return vaultFactories[vaultType]
