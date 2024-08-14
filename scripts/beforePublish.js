c5723442428b3c221d6059b217f71343f9b7cd5b
MikeDiam <info@mdiamond.ru>
2024-08-14 14:45:19 +0300

    [publish build] add build check action

--- scripts/beforePublish.js
+++ scripts/beforePublish.js
@@ -5,9 +5,11 @@ const package = require('../package.json')
 const check = async () => {
   const npmVersion = package.version
   const releaseVersion = execSync(`git ls-remote https://github.com/stakewise/v3-sdk refs/tags/${npmVersion}`, { encoding: 'utf8' })
+    .replace(/.*\//, '')
+    .trim()
 
-  if (package.version !== releaseVersion || !releaseVersion) {
-    console.error(`Error: release version is ${npmVersion}, but should be ${package.version}`)
+  if (npmVersion !== releaseVersion || !releaseVersion) {
+    console.error(`Error: release version is ${releaseVersion}, but should be ${npmVersion}`)
     process.exit(1)
   }
   else {
