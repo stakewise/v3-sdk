6048e1538b752f1cea7987b1ea2dd750c0ac0eb7
MikeDiam <info@mdiamond.ru>
2024-09-27 13:15:56 +0300

    [get config] update get config

--- src/methods/osToken/index.ts
+++ src/methods/osToken/index.ts
@@ -27,7 +27,7 @@ export default {
     */
     getRate,
     /**
-     * @deprecated use sdk.vault.getConfig instead to get data from osTokenConfig
+     * @deprecated use https://github.com/stakewise/v3-sdk/?tab=readme-ov-file#sdkvaultgetvault
      * @description os token ltvPercent and thresholdPercent for provided vault
      * @see https://github.com/stakewise/v3-sdk/?tab=readme-ov-file#sdkostokengetconfig
     */
