57614d4e37f914724e8404cbf12c02de1e71395e
CAst <programmer.andrey@gmail.com>
2024-10-25 14:01:26 +0500

    Boost part 3 (#195)
    * Boost apy (#187)
    
    * [boost-apy] set apy logic
    
    * [boost-apy] change codegen
    
    * [boost-apy] change user stats
    
    * [boost-apy] improve ltv percent logic
    
    * [boost-apy] fix config
    
    * [boost-apy] fix readme
    
    * [boost-apy] remove import
    
    * [boost-part-2] remove boost percent (#189)
    
    * [boost ui] update boost data (#188)
    
    * remove duplicates (#186) (#191)
    
    * refactor checkRestakeOperatorsManagerAccess
    
    * update subgraphUrl logic
    
    * Boost methods (#192)
    
    * remove duplicates (#186)
    
    * refactor checkRestakeOperatorsManagerAccess
    
    * update subgraphUrl logic
    
    * [boost ui] add boost multicall to sdk
    
    * [boost ui] add boost to sdk
    
    * [boost ui] add boost to sdk
    
    * [boost ui] update types
    
    * [boost ui] add boost methods
    
    * [boost ui] add boost methods
    
    * [boost ui] add boost methods
    
    * [boost ui] add boost methods
    
    * [boost ui] update type
    
    * [boost ui] update type
    
    * [boost ui] update permit signature
    
    ---------
    
    Co-authored-by: Kadyr Dzhemaledinov <dfkadyr@gmail.com>
    
    * [boost-part-3] add boost method
    
    * [boost-part-3] fixes
    
    ---------
    
    Co-authored-by: Mike Diamond <info@mdiamond.ru>
    Co-authored-by: Kadyr Dzhemaledinov <dfkadyr@gmail.com>

---
+++ src/methods/vault/requests/getBoost.ts
@@ -0,0 +1,74 @@
+import graphql from '../../../graphql'
+import { wrapAbortPromise } from '../../../modules/gql-module'
+import { validateArgs, apiUrls, BigDecimal, Network, constants } from '../../../utils'
+
+
+type GetBoostInput = {
+  userAddress: string
+  vaultAddress: string
+  options: StakeWise.Options
+  contracts: StakeWise.Contracts
+}
+
+type Output = {
+  shares: bigint
+  percent: number
+  maxMintShares: bigint
+  isProfitable: boolean
+}
+
+const getBoost = async (values: GetBoostInput) => {
+  const { contracts, options, vaultAddress, userAddress } = values
+
+  validateArgs.address({ vaultAddress, userAddress })
+
+  const boost: Output = {
+    shares: 0n,
+    percent: 0,
+    maxMintShares: 0n,
+    isProfitable: false,
+  }
+
+  if ([ Network.Mainnet, Network.Holesky ].includes(options.network)) {
+    const response = await graphql.subgraph.osToken.fetchBoostTokenSharesQuery({
+      url: apiUrls.getSubgraphqlUrl(options),
+      variables: {
+        vaultAddress,
+        userAddress,
+      },
+    })
+
+    const { leverageStrategyPositions, vaults, allocators } = response
+
+    if (!vaults.length) {
+      return boost
+    }
+
+    const { apy, maxBoostApy, osTokenConfig } = vaults[0]
+
+    const stakedAssets = BigInt(allocators[0]?.assets || 0)
+    const ltvPercent = BigInt(osTokenConfig.ltvPercent || 0)
+    const boostShares = BigInt(leverageStrategyPositions[0]?.osTokenShares || 0)
+
+    const maxMintAssets = stakedAssets * ltvPercent / constants.blockchain.amount1
+    const maxMintShares = await contracts.base.mintTokenController.convertToShares(maxMintAssets)
+
+    const boostPercent = maxMintShares ? (
+      new BigDecimal(boostShares)
+        .multiply(100)
+        .divide(maxMintShares)
+        .decimals(2)
+        .toNumber()
+    ) : 0
+
+    boost.shares = boostShares
+    boost.percent = boostPercent
+    boost.maxMintShares = maxMintShares
+    boost.isProfitable = maxBoostApy > apy
+  }
+
+  return boost
+}
+
+
+export default wrapAbortPromise<GetBoostInput, Output>(getBoost)
