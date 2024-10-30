504fd4c699d6e5b0a078eeda8991f30eeef1496a
MikeDiam <info@mdiamond.ru>
2024-10-30 13:40:58 +0300

    [unboost ui] update unboost ui

--- src/methods/utils/getBoostApy.ts
+++ src/methods/utils/getBoostApy.ts
@@ -5,27 +5,21 @@ type Input = {
   value: bigint
   vaultAPY: number
   maxBoostAPY: number
-  boostPercent: number
   maxMintShares: bigint
 }
 
-const getBoostApy = ({ value, vaultAPY, maxBoostAPY, boostPercent, maxMintShares }: Input) => {
-  const apyDiff = maxBoostAPY - vaultAPY
+const getBoostApy = ({ value, vaultAPY, maxBoostAPY, maxMintShares }: Input) => {
+  const boostPercent = maxMintShares
+    ? new BigDecimal(value).multiply(100).divide(maxMintShares)
+    : 0n
 
-  const balance = boostPercent ? (
-    new BigDecimal(maxMintShares)
-      .divide(100)
-      .multiply(boostPercent)
-      .toString()
-  ) : maxMintShares
-
-  const boostAPY = new BigDecimal(apyDiff)
-    .multiply(value)
-    .divide(balance)
+  return new BigDecimal(maxBoostAPY)
+    .minus(vaultAPY)
+    .multiply(boostPercent)
+    .divide(100)
+    .plus(vaultAPY)
     .decimals(2)
     .toNumber()
-
-  return Number((boostAPY +  vaultAPY).toFixed(2))
 }
 
 
