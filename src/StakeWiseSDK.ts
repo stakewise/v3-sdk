d864c884e2ef1a0e4fd94e130673d5d92d52ccda
dfkadyr-stakewise <kadyr@stakewise.io>
2024-11-08 12:28:51 +0300

    improve isDepositWithMint in vaultMulticall

--- src/StakeWiseSDK.ts
+++ src/StakeWiseSDK.ts
@@ -13,9 +13,8 @@ import {
 
 type GetVaultFactoryInput = { vaultType?: VaultType, isErc20?: boolean }
 
-type VaultMulticallInput = Pick<Parameters<typeof vaultMulticall>[0], 'request' | 'userAddress' | 'vaultAddress'> & {
-  isDepositWithMint?: boolean
-}
+type VaultMulticallInput = Pick<Parameters<typeof vaultMulticall>[0], 'request' | 'userAddress' | 'vaultAddress'>
+
 type RewardSplitterMulticallInput = Pick<Parameters<typeof rewardSplitterMulticall>[0], 'request' | 'userAddress' | 'vaultAddress'> & {
   rewardSplitterAddress: string
 }
@@ -67,16 +66,16 @@ class StakeWiseSDK {
   }
 
   async vaultMulticall<T extends unknown>(values: VaultMulticallInput) {
-    const { userAddress, vaultAddress, request, isDepositWithMint } = values
+    const { userAddress, vaultAddress, request } = values
 
-    const { isBlocklist, isPrivate, isRestake } = await this.vault.getVault({ vaultAddress })
+    const { isBlocklist, isPrivate, isRestake, version } = await this.vault.getVault({ vaultAddress })
 
     const vaultContract = this.contracts.helpers.createVault({
       options: {
         isPrivate,
         isRestake,
         isBlocklist,
-        isDepositWithMint,
+        isDepositWithMint: version >= 3,
         chainId: this.config.network.chainId,
       },
       vaultAddress,
