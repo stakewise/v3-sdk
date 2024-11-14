4a749d2da166b1aa0d95e82274ba52addb77d27d
CAst <programmer.andrey@gmail.com>
2024-11-14 16:12:08 +0500

    Rewards data (#207)
    * [rewards-data] add merge rewards & fiat helper
    
    * [rewards-data] change reqests
    
    * [rewards-data] create calculateUserStats
    
    * [rewards-data] readme
    
    * [rewards-data] readme
    
    * [rewards-data] fix

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from './utils/enums'
 export { createContract } from './contracts'
 export { default as StakeWiseSDK } from './StakeWiseSDK'
 export { wrapAbortPromise, AbortPromise } from './modules/gql-module'
+export { BigDecimal, configs, getGas, createProvider, mergeRewardsFiat, calculateUserStats } from './utils'
 
 export {
   configs,
