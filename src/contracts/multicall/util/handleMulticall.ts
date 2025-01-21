dce0fa39025441d95c70ae91141f90ca40bab8e8
Andrey Kopylov <programmer.andrey@gmail.com>
2025-01-21 17:37:51 +0500

    new

--- src/contracts/multicall/util/handleMulticall.ts
+++ src/contracts/multicall/util/handleMulticall.ts
@@ -15,16 +15,16 @@ const handleMulticall = async ({ contract, multicallParams }: Input) => {
   // Even though ethers tries to find the best price for gas, sometimes it's
   // not enough and the transaction breaks down and users lose money for gas.
   // Adding 10% to the gas limit
-  // if (isSoloCall) {
-  //   const { method, args } = multicallParams[0]
+  if (isSoloCall) {
+    const { method, args } = multicallParams[0]
 
-  //   // @ts-ignore: no types to describe
-  //   const estimatedGas = await contract[method].estimateGas(...args)
-  //   const gasLimit = estimatedGas * 110n / 100n
+    // @ts-ignore: no types to describe
+    const estimatedGas = await contract[method].estimateGas(...args)
+    const gasLimit = estimatedGas * 110n / 100n
 
-  //   // @ts-ignore: no types to describe
-  //   return contract[method](...args, { gasLimit })
-  // }
+    // @ts-ignore: no types to describe
+    return contract[method](...args, { gasLimit })
+  }
 
   // const estimatedGas = await contract.multicall.estimateGas(calls)
   // const gasLimit = estimatedGas * 110n / 100n
