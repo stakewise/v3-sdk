2a837f4631f1f259a9241c994c0ca2e52c2e2ff7
Mike Diamond <info@mdiamond.ru>
2025-02-05 18:01:53 +0300

    [boost improve] updated boost (#246)
    * [boost improve] updated boost
    
    * [boost improve] update readme

--- src/graphql/subgraph/boost/index.ts
+++ src/graphql/subgraph/boost/index.ts
@@ -1,5 +1,8 @@
 export { fetchBoostMainDataQuery } from './boostMainDataQuery.graphql'
 export type { BoostMainDataQueryPayload, BoostMainDataQueryVariables } from './boostMainDataQuery.graphql'
 
+export { fetchAavePositionsQuery } from './aavePositionsQuery.graphql'
+export type { AavePositionsQueryPayload, AavePositionsQueryVariables } from './aavePositionsQuery.graphql'
+
 export { fetchBoostQueuePositionsQuery } from './boostQueuePositionsQuery.graphql'
 export type { BoostQueuePositionsQueryPayload, BoostQueuePositionsQueryVariables } from './boostQueuePositionsQuery.graphql'
