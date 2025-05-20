508f059826f19d1978d89557ab09455ba5f97a53
Kadyr Dzhemaledinov <kadyr@stakewise.io>
2025-05-20 11:57:14 +0300

    [export-rewards] improve limit range (#279)
    * [export-rewards] improve limit range
    
    * [export-rewards] improve limit range

--- src/methods/vault/requests/getUserRewards/index.ts
+++ src/methods/vault/requests/getUserRewards/index.ts
@@ -13,8 +13,9 @@ type GetUserRewardsInput = {
 
 const calculateLimit = (dateTo: number, dateFrom: number): number => {
   const millisecondsInDay = 1000 * 60 * 60 * 24
+  const rawDays = (dateTo - dateFrom) / millisecondsInDay
 
-  return Math.floor((dateTo - dateFrom) / millisecondsInDay)
+  return Math.ceil(rawDays)
 }
 
 const getUserRewards = async (input: GetUserRewardsInput): Promise<MergedReward[]> => {
@@ -36,6 +37,10 @@ const getUserRewards = async (input: GetUserRewardsInput): Promise<MergedReward[
     : subgraphUrl
 
   const limit = calculateLimit(dateTo, dateFrom)
+
+  if (limit > 1_000) {
+    throw new Error('Selected date range exceeds the limit. The maximum allowed span is 1000 days. Please choose a shorter range.')
+  }
 
   const timestampTo = String(dateTo * 1_000)
   const timestampFrom = String(dateFrom * 1_000)
