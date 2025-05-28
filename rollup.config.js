08ca66ded72879cbd35cfd25a9f5eebc583cfff6
dfkadyr-stakewise <kadyr@stakewise.io>
2025-05-28 15:01:05 +0300

    [packages] update dependencies and refactor imports to use ES modules

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
-import pkg from './package.json'
+import pkg from './package.json' with { type: 'json' }
 
 
 const config = [
