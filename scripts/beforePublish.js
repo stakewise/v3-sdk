e1d462daad4b5b7e887ef62006d8d4c3e00ecfaf
Alex Gavrilov <alexgavrilov939@gmail.com>
2025-07-10 23:11:23 +0200

    cjs support minor fixes

--- scripts/beforePublish.js
+++ scripts/beforePublish.js
@@ -4,12 +4,12 @@ import pkg from '../package.json' with { type: 'json' }
 
 const check = async () => {
   const npmVersion = pkg.version
-  const releaseVersion = execSync(`git ls-remote https://github.com/stakewise/v3-sdk refs/tags/${npmVersion}`, { encoding: 'utf8' })
+  const releaseVersion = execSync(`git ls-remote https://github.com/AlexGavrilov939/stakewise-v3-sdk-cjs refs/tags/${npmVersion}`, { encoding: 'utf8' })
     .replace(/.*\//, '')
     .trim()
 
   if (npmVersion !== releaseVersion || !releaseVersion) {
-    console.error(`Please add ${npmVersion} release to Github https://github.com/stakewise/v3-sdk/releases/new`)
+    console.error(`Please add ${npmVersion} release to Github https://github.com/AlexGavrilov939/stakewise-v3-sdk-cjs/releases/new`)
     process.exit(1)
   }
   else {
