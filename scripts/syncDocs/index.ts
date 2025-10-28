56e453db50c6e158a64fb93d80c49667150878e4
Andrey Kopylov <programmer.andrey@gmail.com>
2025-10-28 17:10:09 +0500

    [sync-docs] create sync logic and git action

---
+++ scripts/syncDocs/index.ts
@@ -0,0 +1,111 @@
+import path from 'path'
+import time from 'dayjs'
+import fs from 'fs-extra'
+import { glob } from 'glob'
+import simpleGit from 'simple-git'
+
+
+const token = process.env.TOKEN
+const srcPath = `${process.cwd()}/src`
+const docsPath = `${process.cwd()}/docs`
+const docsRepoUrl = `https://${token}@github.com/stakewise/stakewise-docs.git`
+
+const log = {
+  success: (message: string) => console.log(`\x1b[32m✅ ${message}\x1b[0m\n`),
+  info: (message: string) => console.log(`\x1b[36m${message}\x1b[0m\n`),
+  error: (message: string) => console.log(`\x1b[31m❌ ${message}\x1b[0m\n`)
+}
+
+const changeTargetPath = (path: string) => path
+  .replace('methods/', '')
+  .replace('requests/', '')
+
+;(async () => {
+  const git = simpleGit();
+
+  log.info('🤖 Start of documentation synchronization...')
+
+  try {
+    if (!token) {
+      throw new Error('Emtry token!')
+    }
+
+    const isExist = await fs.pathExists(docsPath)
+
+    if (isExist) {
+      log.info('📁 Docs folder already exist.')
+
+      await git.cwd(docsPath).pull('origin', 'main')
+
+      log.success('The documentation repository was updated.')
+    }
+    else {
+      await git.clone(docsRepoUrl, docsPath)
+
+      log.success('The documentation repository has been cloned.')
+    }
+
+    const sourceFiles = await glob(['**/*.md', '**/*.mdx'], {
+      cwd: srcPath
+    })
+
+    log.info(`🔢 Found ${sourceFiles.length} files to sync`)
+
+    await fs.emptyDir(`${docsPath}/docs/sdk`)
+
+    log.info(`🧹 SDK folder in docs has been cleaned up.`)
+
+    const branchName = 'sync-test'
+    const branches = await git.branch()
+
+    const isBranchExist = (
+      branches.all.includes(branchName)
+      || branches.all.includes(`remotes/origin/${branchName}`)
+    )
+
+    if (isBranchExist) {
+      log.info(`The branch 'sync-test' already exist.`)
+
+      await git.checkout(branchName)
+      await git.pull()
+
+      log.success(`Checkout to 'sync-test' and pull success.`)
+    }
+    else {
+      await git.checkoutLocalBranch(branchName)
+
+      log.success(`Create branch 'sync-test' success.`)
+    }
+
+    for (const file of sourceFiles) {
+      const sourceFile = `${srcPath}/${file}`
+      const targetFile = changeTargetPath(`${docsPath}/docs/sdk/${file}`)
+
+      await fs.ensureDir(path.dirname(targetFile))
+      await fs.copy(sourceFile, targetFile)
+
+      log.info(`Copied: ${file.replace(/.*\/(.*)\.(mdx?)$/, '$1.$2')}`)
+    }
+
+    await git
+      .addConfig('user.name', 'github-actions[bot]')
+      .addConfig('user.email', 'github-actions[bot]@users.noreply.github.com')
+      .addConfig('commit.gpgsign', 'true');
+
+    log.success(`GitHub Commit signer is setted.`)
+
+    await git.add('.')
+
+    const date = time().format('DD.MM.YYYY')
+    await git.commit(`Sync SDK documentation test [${date}]`)
+
+    log.success('Changes are commited.')
+
+    await git.push('origin', branchName)
+
+    log.success("Changes are pushed to 'sync-test' branch.")
+  }
+  catch (error) {
+    log.error(`${error}`)
+  } 
+})()
