ed81c2f485ead0e93ec1b61c9e9afe628fa8fe92
Andrey Kopylov <programmer.andrey@gmail.com>
2025-12-19 17:44:52 +0500

    [income-field] fix type 2

--- src/services/vault/requests/getValidators/types.ts
+++ src/services/vault/requests/getValidators/types.ts
@@ -1,9 +1,10 @@
 import type { ValidatorsQueryPayload } from '../../../../graphql/backend/vault'
 
 
-type ValidatorsPayload = Pick<ValidatorsQueryPayload['vaultValidators'][number], 'apy' | 'publicKey' | 'income'>
+type ValidatorsPayload = Pick<ValidatorsQueryPayload['vaultValidators'][number], 'apy' | 'publicKey'>
 
 export type ModifiedValidators = Array<ValidatorsPayload & {
   createdAt: number
+  earned: string
   link: string
 }>
