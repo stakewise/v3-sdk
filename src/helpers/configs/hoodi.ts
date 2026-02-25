253565e62cbc7d80c586a65c7fa7207baae859ac
CAst <programmer.andrey@gmail.com>
2026-02-25 20:40:24 +0500

    [new-metavaults-addresses] set new data (#346)

--- src/helpers/configs/hoodi.ts
+++ src/helpers/configs/hoodi.ts
@@ -45,10 +45,11 @@ export default {
       blocklistVault: '0x608d8Ca6916b96edf63Dd429e62Fe1366ae6f3B5',
       erc20BlocklistVault: '0x39c6eef5f955bcC280966504bc5c82F2394Fa368',
 
-      metavault: '0x3C97239F12A94b914361F41B51869218a16c2026',
-      erc20Metavault: '0x94E1adDa4f5ca28aA2549aA9e18D89E3184F8b70',
-      privateMetavault: '0xe914Cfb2EB9e2d208b19b3E280A3096479491eAd',
-      erc20PrivateMetavault: '0x6ba3d8DF1ECa6201cc30D5D2E58294B23DAFF221',
+      metavault: '0x721C63fc53C432FC0feA4e0775e7ABFd29983347',
+      erc20Metavault: '0x0aca1A126d0636bf74086DD077b47366a1353D4D',
+
+      privateMetavault: '0xa11f61D2687993CF29B6aEb8f9EFb3147F34Dbb6',
+      erc20PrivateMetavault: '0xCCf6D110181330fB8b62de8FA37fd0a905665376',
     },
     special: {
       balancedCurator: '0xD30E7e4bDbd396cfBe72Ad2f4856769C54eA6b0b',
