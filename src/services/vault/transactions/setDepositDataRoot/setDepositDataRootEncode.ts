12152bd43385d5ba630487581736a3a2c759d41f
dfkadyr <dfkadyr@gmail.com>
2026-04-08 10:22:40 +0300

    refactor gas & encode in setDepositData transactions

--- src/services/vault/transactions/setDepositDataRoot/setDepositDataRootEncode.ts
+++ src/services/vault/transactions/setDepositDataRoot/setDepositDataRootEncode.ts
@@ -1,11 +1,22 @@
 import { commonLogic } from './common'
 import type { SetDepositDataRootInput } from './types'
+import getVaultVersion from '../../requests/getVaultVersion'
 
 
 const setDepositDataRootEncode = async (values: SetDepositDataRootInput) => {
+  const { vaultAddress, depositDataRoot, contracts } = values
+
+  const { isV1Version } = await getVaultVersion(values)
+
+  if (isV1Version) {
+    const vaultContract = contracts.helpers.createVault({ vaultAddress })
+
+    return vaultContract.setValidatorsRoot.populateTransaction(depositDataRoot)
+  }
+
   const contract = commonLogic(values)
 
-  return contract.setDepositDataRoot.populateTransaction(values.vaultAddress, values.depositDataRoot)
+  return contract.setDepositDataRoot.populateTransaction(vaultAddress, depositDataRoot)
 }
 
 
