266533ee043021eaec7d6cc32ed20deec733f7a1
CAst <programmer.andrey@gmail.com>
2026-04-16 16:35:31 +0500

    [handle-429] add retry-after header support (#364)

--- src/helpers/createProvider/getFetchRequest.ts
+++ src/helpers/createProvider/getFetchRequest.ts
@@ -1,5 +1,8 @@
-import { FetchRequest, FetchResponse } from 'ethers'
+import { FetchRequest } from 'ethers'
 
+
+const baseCooldownMs = 10_000
+const maxCooldownMs = 60_000
 
 const getFetchRequest = (_url: string | StakeWise.UrlWithHeaders) => {
   const url = typeof _url === 'string' ? _url : _url.url
@@ -20,20 +23,52 @@ const getFetchRequest = (_url: string | StakeWise.UrlWithHeaders) => {
     maxAttempts: 2,
   })
 
+  // Throttle state: on 429 we set a cooldown that doubles
+  // (10s, 20s, 40s, 60s max) and resets on a successful request.
+  // If the server sends a Retry-After header, we use that value instead.
+  let throttledUntil = 0
+  let cooldownMs = baseCooldownMs
+
+  fetchRequest.preflightFunc = async (request) => {
+    const remaining = throttledUntil - Date.now()
+
+    if (remaining > 0) {
+      await new Promise((resolve) => setTimeout(resolve, remaining))
+    }
+
+    return request
+  }
+
+  // processFunc is called after retryFunc, including for 429 when retryFunc returns false
   fetchRequest.processFunc = async (_, response) => {
-    if (response.statusCode === 429) {
-      // mutate the response to 500 to start an additional node
-      // https://github.com/ethers-io/ethers.js/blob/main/src.ts/utils/fetch.ts#L519
-      return new FetchResponse(500, '', response.headers, null)
+    if (response.statusCode !== 429) {
+      throttledUntil = 0
+      cooldownMs = baseCooldownMs
     }
 
     return response
   }
 
   fetchRequest.retryFunc = async (_, response) => {
-    // stop retry attempts
-    // https://github.com/ethers-io/ethers.js/blob/main/src.ts/utils/fetch.ts#L556
-    return response.statusCode !== 429
+    if (response.statusCode === 429) {
+      const retryAfter = response.headers['retry-after']
+
+      const retryAfterMs = retryAfter
+        ? Number(retryAfter) * 1_000
+        : 0
+
+      if (retryAfterMs > 0) {
+        throttledUntil = Date.now() + retryAfterMs
+      }
+      else {
+        throttledUntil = Date.now() + cooldownMs
+        cooldownMs = Math.min(maxCooldownMs, cooldownMs * 2)
+      }
+
+      return false
+    }
+
+    return true
   }
 
   return fetchRequest
