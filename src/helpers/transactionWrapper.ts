e4b1cf8d403fd20f05e03dcdbace5a3c627f09af
Kadyr Dzhemaledinov <dfkadyr@gmail.com>
2026-04-24 12:47:43 +0300

    Merge main sdk (#366)
    * Remove sync docs git action (#354)
    
    * [remove-sync-docs] remove git action and change script
    
    * change git action
    
    * update GitHub Actions to use latest checkout and cache actions (#356)
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    * Add script to check documentation links (#357)
    
    * add script to check documentation links
    
    * fix after review
    
    * rename documentation files and improve slug validation in checkDocLinks
    
    * add category configuration for Quick Start
    
    * improve slug processing and include _category_.json in documentation file search
    
    * 4.2.3 (#358)
    
    * [fix-encode] fix transactionWrapper (#360)
    
    * up-version (#361)
    
    * [handle-429] add retry-after header support (#364)
    
    * fix
    
    ---------
    
    Co-authored-by: CAst <programmer.andrey@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/helpers/transactionWrapper.ts
+++ src/helpers/transactionWrapper.ts
@@ -1,13 +1,26 @@
+interface TransactionLike {
+  (...args: any[]): any
+  encode: (...args: any[]) => any
+  estimateGas: (...args: any[]) => Promise<bigint>
+}
+
 /**
  * @description Prevents transaction invocation if provider is not passed to the SDK instance
 */
-const transactionWrapper = <T extends unknown>(params: StakeWise.CommonParams, method: T): T => {
+const transactionWrapper = <T extends TransactionLike>(params: StakeWise.CommonParams, method: T): T => {
   const isReadOnlyProvider = !params.options.provider
 
   if (isReadOnlyProvider) {
-    return (() => {
+    const block = () => {
       throw new Error('To send this transaction, please provide BrowserProvider to the StakeWiseSDK')
-    }) as T
+    }
+
+    const newMethod = block as unknown as T
+
+    newMethod.estimateGas = block as T['estimateGas']
+    newMethod.encode = method.encode
+
+    return newMethod
   }
 
   return method
