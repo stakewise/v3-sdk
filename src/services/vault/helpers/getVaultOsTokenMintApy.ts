03175593028314330f94dfac7dedb26760fc0757
dfkadyr <dfkadyr@gmail.com>
2026-09-10 13:39:44 +0300

    improve after review (part 4)

--- src/services/vault/helpers/getVaultOsTokenMintApy.ts
+++ src/services/vault/helpers/getVaultOsTokenMintApy.ts
@@ -1,15 +1,17 @@
 import { BigDecimal, constants } from '../../../helpers'
 
 
+const fullPercent = 10_000
+
 const getVaultOsTokenMintApy = (osTokenApy: number, feePercent: number, ltvPercent: bigint): number => {
-  if (ltvPercent === 0n) {
+  if (ltvPercent === 0n || feePercent >= fullPercent) {
     return 0
   }
 
   return new BigDecimal(osTokenApy)
     .multiply(feePercent)
     .multiply(constants.blockchain.amount1)
-    .divide(10000 - feePercent)
+    .divide(fullPercent - feePercent)
     .divide(ltvPercent)
     .toNumber()
 }
